41d4bab254dff03e7282cc93783df1bb699c53bd
lee <@wanchun.li>
2020-10-03 15:41:56 +0800

    https

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const Koa = require('koa')
 const path = require('path')
 const static = require('koa-static')
+const sslify = require('koa-ssligy')
+const fs = require('fs')
 const app = new Koa()
-const staticPath = './static'
 
+const port = 443
+const staticPath = './static'
+const options = {
+  key: fs.readFileSync('/root/.acme.sh/meizizi.me/meizizi.me.key'),
+  cert: fs.readFileSync('/root/.acme.sh/meizizi.me/meizizi.me.cer'),
+}
+app.use(sslify())
 app.use(static(
     path.join( __dirname,  staticPath)
 ))
@@ -11,6 +19,11 @@ app.use( async ( ctx ) => {
     ctx.body = 'hello world'
 })
 
-app.listen(80, () => {
-    console.log('server is starting at port 3000')
+// app.listen(port, () => {
+//     console.log('server is starting at port ' + port)
+// })
+https.createServer(options, app.callback()).listen(port, (err) => {
+  if (err) {
+    console.log('服务启动出错', err);
+  }
 })
