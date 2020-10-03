776fe89c6566ab46ed46a3304dbaadf5afedc3c5
lee <@wanchun.li>
2020-10-03 15:54:12 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const Koa = require('koa')
 const path = require('path')
+const https = require('https')
 const static = require('koa-static')
-const sslify = require('koa-sslify')
+const sslify = require('koa-sslify').default
 const fs = require('fs')
 const app = new Koa()
 
-const port = 443
+
+const port = 80
+const httpsPort = 443
 const staticPath = './static'
 const options = {
   key: fs.readFileSync('/root/.acme.sh/meizizi.me/meizizi.me.key'),
   cert: fs.readFileSync('/root/.acme.sh/meizizi.me/meizizi.me.cer'),
 }
+
 app.use(sslify())
 app.use(static(
     path.join( __dirname,  staticPath)
@@ -19,11 +23,13 @@ app.use( async ( ctx ) => {
     ctx.body = 'hello world'
 })
 
-// app.listen(port, () => {
-//     console.log('server is starting at port ' + port)
-// })
-https.createServer(options, app.callback()).listen(port, (err) => {
+app.listen(port, () => {
+    console.log('server is starting at port ' + port)
+})
+https.createServer(options, app.callback()).listen(httpsPort, (err) => {
   if (err) {
     console.log('服务启动出错', err);
+  } else {
+    console.log('https server is starting at port ' + httpsPort)
   }
 })
